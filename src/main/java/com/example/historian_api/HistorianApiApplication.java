d794cb210b3a395f06fb89b2eebedd5650f3ca60
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-10-09 09:06:33 +0200

    Add initial dependencies

---
+++ src/main/java/com/example/historian_api/HistorianApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.historian_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistorianApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistorianApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/historian_api/HistorianApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.historian_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorianApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
