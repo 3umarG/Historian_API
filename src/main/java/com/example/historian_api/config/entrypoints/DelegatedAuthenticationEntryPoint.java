29691973d75edd4d88c47512e279e91f932b0a09
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-10-10 06:42:11 +0200

    Add main application configuration for security.

---
+++ src/main/java/com/example/historian_api/config/entrypoints/DelegatedAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.example.historian_api.config.entrypoints;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component("delegatedAuthenticationEntryPoint")
+public class DelegatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(DelegatedAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException {
+        // TODO : that will give the handling process to the @ControllerAdvice
+        LOGGER.warn("Enter the `Commence` method");
+        LOGGER.error(authException.getMessage());
+        resolver.resolveException(request, response, null, authException);
+
+    }
+}
