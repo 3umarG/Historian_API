6d916c7eb0926dd691256b7c19b887f518e7f3b2
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-10-12 00:28:19 +0200

    Add repository for replies entity.

---
+++ src/main/java/com/example/historian_api/repositories/CommentRepliesRepository.java
@@ -0,0 +1,13 @@
+package com.example.historian_api.repositories;
+
+import com.example.historian_api.entities.posts.CommentReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepliesRepository extends JpaRepository<CommentReply, Integer> {
+
+    List<CommentReply> findAllByComment_IdOrderByCreatedAtAsc(Integer commentId);
+}
