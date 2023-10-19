90df1635205976daaca6cdbf63b99313691ded06
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-10-19 13:51:09 +0200

    Add swagger doc for remote server.

--- src/main/java/com/example/historian_api/HistorianApiApplication.java
+++ src/main/java/com/example/historian_api/HistorianApiApplication.java
@@ -1,9 +1,21 @@
 package com.example.historian_api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(
+        url = "https://historian-api.up.railway.app",
+        description = "Project for Historian API"
+)}
+)
+@CrossOrigin(
+        origins = "*",
+        maxAge = 3600,
+        allowedHeaders = "*")
 public class HistorianApiApplication {
 
     public static void main(String[] args) {
