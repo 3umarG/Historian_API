1f0160670b2811b5997c11f8746f514abaf48720
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-10-24 15:00:18 +0200

    Add `OnDelete` `cascade` for feedbacks in students.

--- src/main/java/com/example/historian_api/entities/users/Student.java
+++ src/main/java/com/example/historian_api/entities/users/Student.java
@@ -2,6 +2,7 @@ package com.example.historian_api.entities.users;
 
 import com.example.historian_api.dtos.requests.RegisterStudentRequestDto;
 import com.example.historian_api.entities.courses.*;
+import com.example.historian_api.entities.feedbacks.Feedback;
 import com.example.historian_api.entities.posts.Bookmark;
 import com.example.historian_api.entities.posts.Comment;
 import com.example.historian_api.entities.posts.Like;
@@ -119,6 +120,13 @@ public class Student implements UserDetails {
     )
     @OnDelete(action = OnDeleteAction.CASCADE)
     private List<GradeQuizResult> gradeQuizResults = new ArrayList<>();
+
+    @OneToMany(
+            mappedBy = "student",
+            cascade = CascadeType.ALL
+    )
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<Feedback> feedbacks = new ArrayList<>();
 
     public Student(String name,
                    String deviceSerial,
