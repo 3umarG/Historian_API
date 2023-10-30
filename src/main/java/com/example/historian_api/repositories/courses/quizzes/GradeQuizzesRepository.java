c10a5cc76df1a665d7601712fc666239d34072f0
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-10-30 16:29:55 +0200

    Add new condition for retrieving all non-hidden quizzes.

--- src/main/java/com/example/historian_api/repositories/courses/quizzes/GradeQuizzesRepository.java
+++ src/main/java/com/example/historian_api/repositories/courses/quizzes/GradeQuizzesRepository.java
@@ -20,7 +20,7 @@ public interface GradeQuizzesRepository extends JpaRepository<GradeQuiz, Integer
                    "from grade_quizzes quizzes " +
                    "         left join grade_quizzes_results results " +
                    "                   on quizzes.id = results.quiz_id and results.student_id = ?2 " +
-                   "where quizzes.grade_id = ?1 " +
+                   "where quizzes.grade_id = ?1 and quizzes.is_hidden = false " +
                    "order by quizzes.id",
             nativeQuery = true)
     List<GradeQuizProjection> getAllQuizzesForStudentByGradeId(Integer gradeId, Integer studentId);
