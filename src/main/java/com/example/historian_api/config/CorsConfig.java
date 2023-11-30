f584c8d63e6a49475f1caea2c93c69e4964576ed
MahmoudMatar <mahmoudmatar49@gmail.com>
2023-11-30 17:10:23 +0200

    update cross

--- src/main/java/com/example/historian_api/config/CorsConfig.java
+++ src/main/java/com/example/historian_api/config/CorsConfig.java
@@ -2,18 +2,46 @@ package com.example.historian_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // You can configure specific origins
-        configuration.addAllowedMethod("*"); // You can configure specific HTTP methods
-        configuration.addAllowedHeader("*"); // You can configure specific headers
+
+        // Define specific allowed origins
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",  // Your local development origin
+                "https://historian-api.up.railway.app"  // Your remote server origin
+                // Add more origins as needed
+        ));
+
+        // Define specific allowed HTTP methods
+        configuration.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()
+                // Add more methods as needed
+        ));
+
+        // Define specific allowed headers
+        configuration.setAllowedHeaders(Arrays.asList(
+                "Authorization",
+                "Content-Type"
+                // Add more headers as needed
+        ));
+
+        configuration.setMaxAge(3600L);
+
+        // Allow credentials (e.g., cookies, authentication)
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -22,3 +50,4 @@ public class CorsConfig {
         return source;
     }
 }
+
